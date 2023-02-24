7e24af2241e9ce843b501730cb89718a4b98fefb
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 04:05:30 +0900

    Add files via upload

---
+++ JavaScript_practiceI_string and number_let's calculate.js
@@ -0,0 +1,8 @@
+// Please print sum of 5 and 3.
+console.log(5+3);
+
+// Please print the value that is 20 minus 8.
+console.log(20-8);
+
+// Please print "4 + 5" as a string.
+console.log("4 + 5");
